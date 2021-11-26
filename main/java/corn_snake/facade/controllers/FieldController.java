3373ed2b140c3c11c9d14cdb980beec651371585
albaadani1996 <gusalbkh@student.gu.se>
2021-11-26 18:47:16 +0100

    Add attributes

--- main/java/corn_snake/facade/controllers/FieldController.java
+++ main/java/corn_snake/facade/controllers/FieldController.java
@@ -9,6 +9,453 @@ public class FieldController {
     private ImageView A1;
     @FXML
     private ImageView A2;
+    @FXML
+    private ImageView A3;
+    @FXML
+    private ImageView A4;
+    @FXML
+    private ImageView A5;
+    @FXML
+    private ImageView A6;
+    @FXML
+    private ImageView A7;
+    @FXML
+    private ImageView A8;
+    @FXML
+    private ImageView A9;
+    @FXML
+    private ImageView A10;
+    @FXML
+    private ImageView A11;
+    @FXML
+    private ImageView A12;
+    @FXML
+    private ImageView A13;
+    @FXML
+    private ImageView A14;
+    @FXML
+    private ImageView A15;
+    @FXML
+    private ImageView B1;
+    @FXML
+    private ImageView B2;
+    @FXML
+    private ImageView B3;
+    @FXML
+    private ImageView B4;
+    @FXML
+    private ImageView B5;
+    @FXML
+    private ImageView B6;
+    @FXML
+    private ImageView B7;
+    @FXML
+    private ImageView B8;
+    @FXML
+    private ImageView B9;
+    @FXML
+    private ImageView B10;
+    @FXML
+    private ImageView B11;
+    @FXML
+    private ImageView B12;
+    @FXML
+    private ImageView B13;
+    @FXML
+    private ImageView B14;
+    @FXML
+    private ImageView B15;
+    @FXML
+    private ImageView C1;
+    @FXML
+    private ImageView C2;
+    @FXML
+    private ImageView C3;
+    @FXML
+    private ImageView C4;
+    @FXML
+    private ImageView C5;
+    @FXML
+    private ImageView C6;
+    @FXML
+    private ImageView C7;
+    @FXML
+    private ImageView C8;
+    @FXML
+    private ImageView C9;
+    @FXML
+    private ImageView C10;
+    @FXML
+    private ImageView C11;
+    @FXML
+    private ImageView C12;
+    @FXML
+    private ImageView C13;
+    @FXML
+    private ImageView C14;
+    @FXML
+    private ImageView C15;
+    @FXML
+    private ImageView D1;
+    @FXML
+    private ImageView D2;
+    @FXML
+    private ImageView D3;
+    @FXML
+    private ImageView D4;
+    @FXML
+    private ImageView D5;
+    @FXML
+    private ImageView D6;
+    @FXML
+    private ImageView D7;
+    @FXML
+    private ImageView D8;
+    @FXML
+    private ImageView D9;
+    @FXML
+    private ImageView D10;
+    @FXML
+    private ImageView D11;
+    @FXML
+    private ImageView D12;
+    @FXML
+    private ImageView D13;
+    @FXML
+    private ImageView D14;
+    @FXML
+    private ImageView D15;
+    @FXML
+    private ImageView E1;
+    @FXML
+    private ImageView E2;
+    @FXML
+    private ImageView E3;
+    @FXML
+    private ImageView E4;
+    @FXML
+    private ImageView E5;
+    @FXML
+    private ImageView E6;
+    @FXML
+    private ImageView E7;
+    @FXML
+    private ImageView E8;
+    @FXML
+    private ImageView E9;
+    @FXML
+    private ImageView E10;
+    @FXML
+    private ImageView E11;
+    @FXML
+    private ImageView E12;
+    @FXML
+    private ImageView E13;
+    @FXML
+    private ImageView E14;
+    @FXML
+    private ImageView E15;
+    @FXML
+    private ImageView F1;
+    @FXML
+    private ImageView F2;
+    @FXML
+    private ImageView F3;
+    @FXML
+    private ImageView F4;
+    @FXML
+    private ImageView F5;
+    @FXML
+    private ImageView F6;
+    @FXML
+    private ImageView F7;
+    @FXML
+    private ImageView F8;
+    @FXML
+    private ImageView F9;
+    @FXML
+    private ImageView F10;
+    @FXML
+    private ImageView F11;
+    @FXML
+    private ImageView F12;
+    @FXML
+    private ImageView F13;
+    @FXML
+    private ImageView F14;
+    @FXML
+    private ImageView F15;
+    @FXML
+    private ImageView G1;
+    @FXML
+    private ImageView G2;
+    @FXML
+    private ImageView G3;
+    @FXML
+    private ImageView G4;
+    @FXML
+    private ImageView G5;
+    @FXML
+    private ImageView G6;
+    @FXML
+    private ImageView G7;
+    @FXML
+    private ImageView G8;
+    @FXML
+    private ImageView G9;
+    @FXML
+    private ImageView G10;
+    @FXML
+    private ImageView G11;
+    @FXML
+    private ImageView G12;
+    @FXML
+    private ImageView G13;
+    @FXML
+    private ImageView G14;
+    @FXML
+    private ImageView G15;
+    @FXML
+    private ImageView H1;
+    @FXML
+    private ImageView H2;
+    @FXML
+    private ImageView H3;
+    @FXML
+    private ImageView H4;
+    @FXML
+    private ImageView H5;
+    @FXML
+    private ImageView H6;
+    @FXML
+    private ImageView H7;
+    @FXML
+    private ImageView H8;
+    @FXML
+    private ImageView H9;
+    @FXML
+    private ImageView H10;
+    @FXML
+    private ImageView H11;
+    @FXML
+    private ImageView H12;
+    @FXML
+    private ImageView H13;
+    @FXML
+    private ImageView H14;
+    @FXML
+    private ImageView H15;
+    @FXML
+    private ImageView I1;
+    @FXML
+    private ImageView I2;
+    @FXML
+    private ImageView I3;
+    @FXML
+    private ImageView I4;
+    @FXML
+    private ImageView I5;
+    @FXML
+    private ImageView I6;
+    @FXML
+    private ImageView I7;
+    @FXML
+    private ImageView I8;
+    @FXML
+    private ImageView I9;
+    @FXML
+    private ImageView I10;
+    @FXML
+    private ImageView I11;
+    @FXML
+    private ImageView I12;
+    @FXML
+    private ImageView I13;
+    @FXML
+    private ImageView I14;
+    @FXML
+    private ImageView I15;
+    @FXML
+    private ImageView J1;
+    @FXML
+    private ImageView J2;
+    @FXML
+    private ImageView J3;
+    @FXML
+    private ImageView J4;
+    @FXML
+    private ImageView J5;
+    @FXML
+    private ImageView J6;
+    @FXML
+    private ImageView J7;
+    @FXML
+    private ImageView J8;
+    @FXML
+    private ImageView J9;
+    @FXML
+    private ImageView J10;
+    @FXML
+    private ImageView J11;
+    @FXML
+    private ImageView J12;
+    @FXML
+    private ImageView J13;
+    @FXML
+    private ImageView J14;
+    @FXML
+    private ImageView J15;
+    @FXML
+    private ImageView K1;
+    @FXML
+    private ImageView K2;
+    @FXML
+    private ImageView K3;
+    @FXML
+    private ImageView K4;
+    @FXML
+    private ImageView K5;
+    @FXML
+    private ImageView K6;
+    @FXML
+    private ImageView K7;
+    @FXML
+    private ImageView K8;
+    @FXML
+    private ImageView K9;
+    @FXML
+    private ImageView K10;
+    @FXML
+    private ImageView K11;
+    @FXML
+    private ImageView K12;
+    @FXML
+    private ImageView K13;
+    @FXML
+    private ImageView K14;
+    @FXML
+    private ImageView K15;
+    @FXML
+    private ImageView L1;
+    @FXML
+    private ImageView L2;
+    @FXML
+    private ImageView L3;
+    @FXML
+    private ImageView L4;
+    @FXML
+    private ImageView L5;
+    @FXML
+    private ImageView L6;
+    @FXML
+    private ImageView L7;
+    @FXML
+    private ImageView L8;
+    @FXML
+    private ImageView L9;
+    @FXML
+    private ImageView L10;
+    @FXML
+    private ImageView L11;
+    @FXML
+    private ImageView L12;
+    @FXML
+    private ImageView L13;
+    @FXML
+    private ImageView L14;
+    @FXML
+    private ImageView L15;
+    @FXML
+    private ImageView M1;
+    @FXML
+    private ImageView M2;
+    @FXML
+    private ImageView M3;
+    @FXML
+    private ImageView M4;
+    @FXML
+    private ImageView M5;
+    @FXML
+    private ImageView M6;
+    @FXML
+    private ImageView M7;
+    @FXML
+    private ImageView M8;
+    @FXML
+    private ImageView M9;
+    @FXML
+    private ImageView M10;
+    @FXML
+    private ImageView M11;
+    @FXML
+    private ImageView M12;
+    @FXML
+    private ImageView M13;
+    @FXML
+    private ImageView M14;
+    @FXML
+    private ImageView M15;
+    @FXML
+    private ImageView N1;
+    @FXML
+    private ImageView N2;
+    @FXML
+    private ImageView N3;
+    @FXML
+    private ImageView N4;
+    @FXML
+    private ImageView N5;
+    @FXML
+    private ImageView N6;
+    @FXML
+    private ImageView N7;
+    @FXML
+    private ImageView N8;
+    @FXML
+    private ImageView N9;
+    @FXML
+    private ImageView N10;
+    @FXML
+    private ImageView N11;
+    @FXML
+    private ImageView N12;
+    @FXML
+    private ImageView N13;
+    @FXML
+    private ImageView N14;
+    @FXML
+    private ImageView N15;
+    @FXML
+    private ImageView O1;
+    @FXML
+    private ImageView O2;
+    @FXML
+    private ImageView O3;
+    @FXML
+    private ImageView O4;
+    @FXML
+    private ImageView O5;
+    @FXML
+    private ImageView O6;
+    @FXML
+    private ImageView O7;
+    @FXML
+    private ImageView O8;
+    @FXML
+    private ImageView O9;
+    @FXML
+    private ImageView O10;
+    @FXML
+    private ImageView O11;
+    @FXML
+    private ImageView O12;
+    @FXML
+    private ImageView O13;
+    @FXML
+    private ImageView O14;
+    @FXML
+    private ImageView O15;
+
 
 
     private ImageView[] field;
@@ -22,5 +469,231 @@ public class FieldController {
     public FieldController() {
         field = new ImageView[225];
         field[0] = A1;
+        field[1] = A2;
+        field[2] = A3;
+        field[3] = A4;
+        field[4] = A5;
+        field[5] = A6;
+        field[6] = A7;
+        field[7] = A8;
+        field[8] = A9;
+        field[9] = A10;
+        field[10] = A11;
+        field[11] = A12;
+        field[12] = A13;
+        field[13] = A14;
+        field[14] = A15;
+        field[15] = B1;
+        field[16] = B2;
+        field[17] = B3;
+        field[18] = B4;
+        field[19] = B5;
+        field[20] = B6;
+        field[21] = B7;
+        field[22] = B8;
+        field[23] = B9;
+        field[24] = B10;
+        field[25] = B11;
+        field[26] = B12;
+        field[27] = B13;
+        field[28] = B14;
+        field[29] = B15;
+        field[30] = C1;
+        field[31] = C2;
+        field[32] = C3;
+        field[33] = C4;
+        field[34] = C5;
+        field[35] = C6;
+        field[36] = C7;
+        field[37] = C8;
+        field[38] = C9;
+        field[39] = C10;
+        field[40] = C11;
+        field[41] = C12;
+        field[42] = C13;
+        field[43] = C14;
+        field[44] = C15;
+        field[45] = D1;
+        field[46] = D2;
+        field[47] = D3;
+        field[48] = D4;
+        field[49] = D5;
+        field[50] = D6;
+        field[51] = D7;
+        field[52] = D8;
+        field[53] = D9;
+        field[54] = D10;
+        field[55] = D11;
+        field[56] = D12;
+        field[57] = D13;
+        field[58] = D14;
+        field[59] = D15;
+        field[60] = E1;
+        field[61] = E2;
+        field[62] = E3;
+        field[63] = E4;
+        field[64] = E5;
+        field[65] = E6;
+        field[66] = E7;
+        field[67] = E8;
+        field[68] = E9;
+        field[69] = E10;
+        field[70] = E11;
+        field[71] = E12;
+        field[72] = E13;
+        field[73] = E14;
+        field[74] = E15;
+        field[75] = F1;
+        field[76] = F2;
+        field[77] = F3;
+        field[78] = F4;
+        field[79] = F5;
+        field[80] = F6;
+        field[81] = F7;
+        field[82] = F8;
+        field[83] = F9;
+        field[84] = F10;
+        field[85] = F11;
+        field[86] = F12;
+        field[87] = F13;
+        field[88] = F14;
+        field[89] = F15;
+        field[90] = G1;
+        field[91] = G2;
+        field[92] = G3;
+        field[93] = G4;
+        field[94] = G5;
+        field[95] = G6;
+        field[96] = G7;
+        field[97] = G8;
+        field[98] = G9;
+        field[99] = G10;
+        field[100] = G11;
+        field[101] = G12;
+        field[102] = G13;
+        field[103] = G14;
+        field[104] = G15;
+        field[105] = H1;
+        field[106] = H2;
+        field[107] = H3;
+        field[108] = H4;
+        field[109] = H5;
+        field[110] = H6;
+        field[111] = H7;
+        field[112] = H8;
+        field[113] = H9;
+        field[114] = H10;
+        field[115] = H11;
+        field[116] = H12;
+        field[117] = H13;
+        field[118] = H14;
+        field[119] = H15;
+        field[120] = I1;
+        field[121] = I2;
+        field[122] = I3;
+        field[123] = I4;
+        field[124] = I5;
+        field[125] = I6;
+        field[126] = I7;
+        field[127] = I8;
+        field[128] = I9;
+        field[129] = I10;
+        field[130] = I11;
+        field[131] = I12;
+        field[132] = I13;
+        field[133] = I14;
+        field[134] = I15;
+        field[135] = J1;
+        field[136] = J2;
+        field[137] = J3;
+        field[138] = J4;
+        field[139] = J5;
+        field[140] = J6;
+        field[141] = J7;
+        field[142] = J8;
+        field[143] = J9;
+        field[144] = J10;
+        field[145] = J11;
+        field[146] = J12;
+        field[147] = J13;
+        field[148] = J14;
+        field[149] = J15;
+        field[150] = K1;
+        field[151] = K2;
+        field[152] = K3;
+        field[153] = K4;
+        field[154] = K5;
+        field[155] = K6;
+        field[156] = K7;
+        field[157] = K8;
+        field[158] = K9;
+        field[159] = K10;
+        field[160] = K11;
+        field[161] = K12;
+        field[162] = K13;
+        field[163] = K14;
+        field[164] = K15;
+        field[165] = L1;
+        field[166] = L2;
+        field[167] = L3;
+        field[168] = L4;
+        field[169] = L5;
+        field[170] = L6;
+        field[171] = L7;
+        field[172] = L8;
+        field[173] = L9;
+        field[174] = L10;
+        field[175] = L11;
+        field[176] = L12;
+        field[177] = L13;
+        field[178] = L14;
+        field[179] = L15;
+        field[180] = M1;
+        field[181] = M2;
+        field[182] = M3;
+        field[183] = M4;
+        field[184] = M5;
+        field[185] = M6;
+        field[186] = M7;
+        field[187] = M8;
+        field[188] = M9;
+        field[189] = M10;
+        field[190] = M11;
+        field[191] = M12;
+        field[192] = M13;
+        field[193] = M14;
+        field[194] = M15;
+        field[195] = N1;
+        field[196] = N2;
+        field[197] = N3;
+        field[198] = N4;
+        field[199] = N5;
+        field[200] = N6;
+        field[201] = N7;
+        field[202] = N8;
+        field[203] = N9;
+        field[204] = N10;
+        field[205] = N11;
+        field[206] = N12;
+        field[207] = N13;
+        field[208] = N14;
+        field[209] = N15;
+        field[210] = O1;
+        field[211] = O2;
+        field[212] = O3;
+        field[213] = O4;
+        field[214] = O5;
+        field[215] = O6;
+        field[216] = O7;
+        field[217] = O8;
+        field[218] = O9;
+        field[219] = O10;
+        field[220] = O11;
+        field[221] = O12;
+        field[222] = O13;
+        field[223] = O14;
+        field[224] = O15;
+
+
     }
 }
