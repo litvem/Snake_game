2e1abc28f7a3a124727fcbc43b31573b96dfa7b3
BaoLindgren <91387607+BaoLindgren@users.noreply.github.com>
2021-11-26 16:25:24 +0100

    Add attributes

--- main/java/corn_snake/facade/controllers/FieldController.java
+++ main/java/corn_snake/facade/controllers/FieldController.java
@@ -1,4 +1,26 @@
 package corn_snake.facade.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class FieldController {
+    @FXML
+    private ImageView A1;
+    @FXML
+    private ImageView A2;
+
+
+    private ImageView[] field;
+
+    @FXML
+    private Label score;
+
+    @FXML
+    private ImageView gameOver;
+
+    public FieldController() {
+        field = new ImageView[225];
+        field[0] = A1;
+    }
 }
