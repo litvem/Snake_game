8545febfa4d7f92a4e7fb58c2e65efc2291f1d00
BaoLindgren <91387607+BaoLindgren@users.noreply.github.com>
2021-12-01 12:01:13 +0100

    Title screen prototype

--- main/java/corn_snake/facade/controllers/MenuController.java
+++ main/java/corn_snake/facade/controllers/MenuController.java
@@ -1,4 +1,35 @@
 package corn_snake.facade.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MenuController {
+
+    @FXML
+    private Label action;
+
+    @FXML
+    public void onExitClick() {
+        System.exit(0);
+    }
+
+    @FXML
+    public void onPlayClick() {
+        action.setText("Play clicked!");
+    }
+
+    @FXML
+    public void onCreditsClick() {
+        action.setText("Credits clicked!");
+    }
+
+    @FXML
+    public void onLeaderboardClick() {
+        action.setText("Leaderboard clicked!");
+    }
+
+    @FXML
+    public void onHowToPlayClick() {
+        action.setText("How to play clicked!");
+    }
 }
