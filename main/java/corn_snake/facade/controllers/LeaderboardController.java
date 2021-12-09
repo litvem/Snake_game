acb58e01e3d08b6449074c3abf83d8c7daa3c272
albaadani1996 <gusalbkh@student.gu.se>
2021-12-09 21:18:29 +0100

    Bind Screens "leaderboard" & "How to Play"

--- main/java/corn_snake/facade/controllers/LeaderboardController.java
+++ main/java/corn_snake/facade/controllers/LeaderboardController.java
@@ -1,4 +1,27 @@
 package corn_snake.facade.controllers;
 
+import corn_snake.util.IO;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LeaderboardController {
+    @FXML
+    private AnchorPane window;
+
+    @FXML
+    private ImageView mainMenuButton;
+
+    @FXML
+    public void onMainMenuClick(MouseEvent event) throws IOException {
+        Stage stage = (Stage) window.getScene().getWindow();
+        IO.loadScene(stage, "menu-view.fxml",MenuController.class);
+    }
+
+
+
 }
