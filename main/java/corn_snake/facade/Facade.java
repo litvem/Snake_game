068edbc753d3be8c4c817f624bd0b1823bb06f55
faisal1201 <faisalsa@student.chalmers.se>
2021-12-10 11:05:21 +0100

    Methods for Facade implemented: public void newField(); public List<Score> getLeaderboard(); public void addScore(String name, int score); public Tile[][] getField(); public void updateField(String command); public int getScore(Score score); public void saveLeaderboard();

--- main/java/corn_snake/facade/Facade.java
+++ main/java/corn_snake/facade/Facade.java
@@ -1,4 +1,43 @@
 package corn_snake.facade;
 
+import corn_snake.back_end.*;
+import java.util.List;
+import corn_snake.util.Json;
+
+
 public class Facade {
+
+    private Leaderboard lb;
+    private Field field;
+
+
+    public void newField(){
+        field= new Field();
+    }
+
+    public List<Score> getLeaderboard(){return lb.getTop10();}
+
+    public void addScore(String name, int score){
+        Score playerScore= new Score(name,score);
+        lb.addScore(playerScore);
+    }
+
+    public Tile[][] getField(){
+        return field.getMatrix();}
+
+    public void updateField(String command) {
+        updateField(command);
+    }
+
+    public int getScore(Score score){
+        return score.getScore();
+    }
+
+    public void saveLeaderboard(){
+        try {
+            Json.dump(getLeaderboard(),"src/main/resources");
+        }catch (Exception e){
+            System.out.println("Directory Invalid");
+        }
+    }
 }
