0b8abfea3985bf0e3b07886143a48d217cbbe639
BaoLindgren <91387607+BaoLindgren@users.noreply.github.com>
2021-12-22 01:11:59 +0100

    Add version variable

--- main/java/corn_snake/Main.java
+++ main/java/corn_snake/Main.java
@@ -5,6 +5,7 @@ import corn_snake.facade.Facade;
 public class Main {
 
     public static final Facade FACADE = new Facade();
+    public static final String VERSION = "Pre-alpha";
 
     public static void main(String[] args) {
         SnakeMain.main(args);
