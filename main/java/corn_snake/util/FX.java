71bd82d4e83ebc3ac7e567bb4e382a8cf91cc2ec
BaoLindgren <91387607+BaoLindgren@users.noreply.github.com>
2021-12-31 15:44:37 +0100

    Refactor

--- main/java/corn_snake/util/FX.java
+++ main/java/corn_snake/util/FX.java
@@ -1,11 +1,17 @@
 package corn_snake.util;
 
 import javafx.animation.ScaleTransition;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
+
 public class FX {
+
     public static final Image
             BACKGROUND = new Image(FX.class.getResource("common_images/TitleBG.png").toExternalForm()),
             HOME = new Image(FX.class.getResource("common_images/HomeButton.png").toExternalForm()),
@@ -20,5 +26,46 @@ public class FX {
         scale.setToX(value);
         scale.setDuration(Duration.millis(50));
         scale.play();
+    }
+
+    /**
+     * Loads a new JavaFX {@link Scene}
+     *
+     * @param currentStage the current {@link Stage} that is used.
+     *                     Bind the container to an FXID to access the stage, example:
+     *                     {@code (Stage) <container>.getScene().getWindow()}
+     * @param file FXML file to load scene from
+     * @param clazz {@link Class} to get resources from
+     * @throws IOException if loading fails
+     */
+    public static void loadScene(Stage currentStage, String file, Class<?> clazz) throws IOException {
+        loadScene(currentStage, file, clazz, null);
+    }
+
+    /**
+     * Loads a new JavaFX {@link Scene} using a CSS style sheet
+     *
+     * @param currentStage the current {@link Stage} that is used.
+     *                     Bind the container to an FXID to access the stage, example:
+     *                     {@code (Stage) <container>.getScene().getWindow()}
+     * @param file FXML file to load scene from
+     * @param clazz {@link Class} to get resources from
+     * @param css CSS to style the scene with
+     * @throws IOException if loading fails
+     */
+    public static void loadScene(Stage currentStage, String file, Class<?> clazz, String css) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                clazz.getResource(file)
+        );
+        Scene scene = new Scene(fxmlLoader.load());
+
+        if (css != null) {
+            scene.getStylesheets().add(clazz.getResource(css).toExternalForm());
+        }
+
+        currentStage.setResizable(false);
+
+        currentStage.setScene(scene);
+        currentStage.show();
     }
 }
--- main/java/corn_snake/util/IO.java
+++ main/java/corn_snake/util/IO.java
@@ -1,68 +1,8 @@
 package corn_snake.util;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class IO {
 
     public static final String EOL = System.lineSeparator();
-
-    /**
-     * Generates a pseudo-random integer with the given parameters as boundaries
-     *
-     * @param lowerBound lower boundary of the interval
-     * @param upperBound upper boundary of the interval
-     * @return a pseudo-randomly generated integer
-     */
-    public static int randInt(int lowerBound, int upperBound) {
-        int range = upperBound - lowerBound;
-        int num = lowerBound + (int) (Math.random() * (range + 1));
-        return num;
-    }
-
-    /**
-     * Loads a new JavaFX {@link Scene}
-     *
-     * @param currentStage the current {@link Stage} that is used.
-     *                     Bind the container to an FXID to access the stage, example:
-     *                     {@code (Stage) <container>.getScene().getWindow()}
-     * @param file FXML file to load scene from
-     * @param clazz {@link Class} to get resources from
-     * @throws IOException if loading fails
-     */
-    public static void loadScene(Stage currentStage, String file, Class<?> clazz) throws IOException {
-        loadScene(currentStage, file, clazz, null);
-    }
-
-    /**
-     * Loads a new JavaFX {@link Scene} using a CSS style sheet
-     *
-     * @param currentStage the current {@link Stage} that is used.
-     *                     Bind the container to an FXID to access the stage, example:
-     *                     {@code (Stage) <container>.getScene().getWindow()}
-     * @param file FXML file to load scene from
-     * @param clazz {@link Class} to get resources from
-     * @param css CSS to style the scene with
-     * @throws IOException if loading fails
-     */
-    public static void loadScene(Stage currentStage, String file, Class<?> clazz, String css) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                clazz.getResource(file)
-        );
-        Scene scene = new Scene(fxmlLoader.load());
-
-        if (css != null) {
-            scene.getStylesheets().add(clazz.getResource(css).toExternalForm());
-        }
-
-        currentStage.setResizable(false);
-
-        currentStage.setScene(scene);
-        currentStage.show();
-    }
 
     public static final String HOW_TO_PLAY =
             "* Click on PLAY." + EOL +
