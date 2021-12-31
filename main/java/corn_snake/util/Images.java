287127603a722733904ed679ba09287a6b90b5d4
BaoLindgren <91387607+BaoLindgren@users.noreply.github.com>
2021-12-31 14:34:45 +0100

    Collect common use images

---
+++ main/java/corn_snake/util/Images.java
@@ -0,0 +1,12 @@
+package corn_snake.util;
+
+import javafx.scene.image.Image;
+
+public class Images {
+    public static final Image
+            BACKGROUND = new Image(Images.class.getResource("common_images/TitleBG.png").toExternalForm()),
+            HOME = new Image(Images.class.getResource("common_images/HomeButton.png").toExternalForm()),
+            LOGO = new Image(Images.class.getResource("common_images/Logo.png").toExternalForm()),
+            SMALL_BOARD = new Image(Images.class.getResource("common_images/SmallBoard.png").toExternalForm()),
+            BOARD = new Image(Images.class.getResource("common_images/Board.png").toExternalForm());
+}
