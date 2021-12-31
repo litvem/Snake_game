5a64e39b2ce1c7ca7a2d0b6810e79dd0852efc22
BaoLindgren <91387607+BaoLindgren@users.noreply.github.com>
2021-12-31 14:32:38 +0100

    Fix text fit

--- main/java/corn_snake/util/IO.java
+++ main/java/corn_snake/util/IO.java
@@ -63,16 +63,22 @@ public class IO {
         currentStage.setScene(scene);
         currentStage.show();
     }
-    public static final String HOW_TO_PLAY = "* Click on PLAY. \n \n* Control the snake in the field by pressing: \n" +
+
+    public static final String HOW_TO_PLAY =
+            "* Click on PLAY." + EOL +
+            "* Control the snake in the field by pressing:" + EOL +
             " w/i (to go up), " +
             "a/j (to go left)," +
             " d/l (to go right) or " +
-            "s/k (to go down). \n\n" +
-            "* To get points: \n" +
-            "Change the direction of the snake so it reaches the fruit on the screen. \n" +
-            "Once the snake eats a fruit, the snake grows by one segment.\n\n" +
-            "* To win the game:\n" +
-            " grow the snake to the point that the whole field is occupied by the snake. \n\n" +
-            "* If the head hits a wall or any segment of the body you lose. \n\n" +
-            "* Good Lock!!!";
+            "s/k (to go down)." + EOL +
+            "* To get points:" + EOL +
+            "Change the direction of the snake" + EOL +
+            "so it reaches the fruit on the screen." + EOL +
+            "Once the snake eats a fruit," + EOL +
+            "the snake grows by one segment." + EOL +
+            "* To win the game:" + EOL +
+            " grow the snake to the point that the whole field " + EOL +
+            "is occupied by the snake." + EOL +
+            "* If the head hits a wall or any segment of the body you lose." + EOL +
+            "* Good Luck!!!";
 }
