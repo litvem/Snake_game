650fa578a62f9997c103bff5e28ccfd909a132c7
BaoLindgren <91387607+BaoLindgren@users.noreply.github.com>
2022-01-06 13:38:23 +0100

    Version update (1.0)

--- main/java/corn_snake/Main.java
+++ main/java/corn_snake/Main.java
@@ -5,7 +5,7 @@ import corn_snake.facade.Facade;
 public class Main {
 
     public static final Facade FACADE = new Facade();
-    public static final String VERSION = "Beta";
+    public static final String VERSION = "1.0";
 
     public static void main(String[] args) {
         SnakeMain.main(args);
